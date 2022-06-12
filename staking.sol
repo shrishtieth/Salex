5e278d74731903eb6b520daa38d039dd7713e144
shrishti.eth <87112793+shrishtieth@users.noreply.github.com>
2022-06-12 23:53:20 +0530

    Update staking.sol

--- staking.sol
+++ staking.sol
@@ -11,17 +11,28 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
  * @author Kei Nakano - Nave Finance
  * @notice scalable reward distribution algorithm
  **/
+interface IStaking{
 
+
+    function getAllHolder() external 
+    returns(address[] memory users, uint256[] memory amount, uint256[] memory 
+    lastStaked, uint256 totalSupply);
+    function addReward(address user, address[] memory tokens, uint256[] memory amountEligible) external;
+    function updateRewardsUser(address token, uint256 amount) external;
+    
+}
 
 contract Staking is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     /* ========== STATE VARIABLES ========== */
 
-    IERC20 public rewardToken;
     IERC20 public stakingToken;
 
     uint256 public constant DIVISION = 10000; // to prevent float calculation
+
+    address public treasury;
+    uint256 public fees;
 
     uint256 public lastRewardRate; // S = 0;
     uint256 public totalStakedAmount; // T = 0;
@@ -42,13 +53,14 @@ contract Staking is Ownable, ReentrancyGuard {
       }
 
 
-    function stake(uint256 _amount) external nonReentrant {
-        require(_amount > 0,  "Cannot stake 0");
-        stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
-        stakedBalance[msg.sender] += _amount; // stake[address] = amount;
+    function stake1(uint256 _amount) external nonReentrant {
+        require(_amount > 100,  "Cannot stake 0");
+        stakingToken.safeTransferFrom(msg.sender, treasury, fees);
+        stakingToken.safeTransferFrom(msg.sender, address(this), _amount-fees);
+        stakedBalance[msg.sender] += _amount-fees; // stake[address] = amount;
         lastTimeStaked[msg.sender] = block.timestamp; 
-        totalStakedAmount += _amount; // T = T + amount;
-        emit Stake(msg.sender, _amount);
+        totalStakedAmount += _amount-fees; // T = T + amount;
+        emit Stake(msg.sender, _amount-fees);
     }
 
     function addReward(address user, address[] memory tokens, uint256[] memory amountEligible) public{
@@ -58,16 +70,18 @@ contract Staking is Ownable, ReentrancyGuard {
         }
     }
 
-    function unstake(uint256 _amount) external nonReentrant {
-        require(_amount > 0, "Cannot withdraw 0");
-
-        uint256 deposited = stakedBalance[msg.sender]; // deposited = stake[address];
-        require(_amount <= deposited, "Withdraw amount is invalid");
-        stakingToken.safeTransfer(msg.sender, _amount);
-        totalStakedAmount -= _amount; // T = T - deposited;
-        stakedBalance[msg.sender] -= _amount; // stake[address] = 0;
-        emit Unstake(msg.sender, _amount);
-    }
+    // function unstake1(uint256 _amount) external nonReentrant {
+    //     require(_amount > 0, "Cannot withdraw 0");
+    //     uint256 deposited = stakedBalance[msg.sender]; // deposited = stake[address];
+    //     require(_amount <= deposited, "Withdraw amount is invalid");
+    //     if(lastTimeStaked[msg.sender] + 2592000 > block.timestamp){
+    //      uint256 fee = 
+    //      stakingToken.safeTransfer(msg.sender, _amount-fee);
+    //     }
+    //     totalStakedAmount -= _amount; // T = T - deposited;
+    //     stakedBalance[msg.sender] -= _amount; // stake[address] = 0;
+    //     emit Unstake(msg.sender, _amount);
+    // }
 
     function unstakeAll() external nonReentrant {
         uint256 deposited = stakedBalance[msg.sender]; // deposited = stake[address];
@@ -79,27 +93,153 @@ contract Staking is Ownable, ReentrancyGuard {
 
 
 
-    function distribute(uint _reward) external onlyOwner {
-        require(_reward > 0, "Cannot distribute 0");
-        require(totalStakedAmount > 0, "No staked amount"); // if T==0 then revert();
-        rewardToken.safeTransferFrom(msg.sender, address(this), _reward);
-        /// r / t can be under 0
-        lastRewardRate += (_reward * DIVISION / totalStakedAmount); // S = S + r / T;
-        emit Distribute(msg.sender, _reward);
+    uint256 public lastClaimTime;
+    uint256 public epoch = 1000;
+    mapping(uint256 => uint256) public succesFullPresales;
+    mapping(uint256 => Reward[]) public rewardsData;
+    address public stakingContract;
+
+    struct Reward{
+       address token ;
+       uint256 amount;
+    }
+
+    function receiveTokens(address token, uint256 amount) public{
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        succesFullPresales[lastClaimTime] += 1;
+        Reward memory newData = Reward({
+          token :token,
+          amount : amount
+        });
+        rewardsData[lastClaimTime].push(newData);
+    }
+
+    function finalizeReward() external {
+        require(block.timestamp> lastClaimTime +epoch,"Time still left");
+        require(succesFullPresales[lastClaimTime]>0,"No successfull presale");
+        if(succesFullPresales[lastClaimTime] < 2){
+               
+               distributePool3(rewardsData[lastClaimTime]);
+               lastClaimTime = block.timestamp;
+        }
+        else if(succesFullPresales[lastClaimTime] > 2 && succesFullPresales[lastClaimTime] <= 10){
+            uint256 count = rewardsData[lastClaimTime].length;
+            Reward[] memory itemsPool= rewardsData[lastClaimTime];
+            Reward[] memory itemsPool1 = new Reward[](count);
+            Reward[] memory itemsPool2 = new Reward[](count);
+            for(uint256 i=0; i< count ; i++){
+            Reward memory newData = Reward({
+             token :itemsPool[i].token,
+             amount : itemsPool[i].amount/2
+            });
+            itemsPool1[i] = newData;
+            itemsPool2[i] = newData;
+            }
+            lastClaimTime = block.timestamp;
+            distributePool2(itemsPool1);
+            distributePool3(itemsPool2);
+
+        }
+        else if(succesFullPresales[lastClaimTime]>10){
+
+            uint256 count = rewardsData[lastClaimTime].length;
+            Reward[] memory itemsPool= rewardsData[lastClaimTime];
+            Reward[] memory itemsPool1 = new Reward[](count);
+            Reward[] memory itemsPool2 = new Reward[](count);
+            Reward[] memory itemsPool3 = new Reward[](count);
+            for(uint256 i=0; i< count ; i++){
+            Reward memory newData = Reward({
+             token :itemsPool[i].token,
+             amount : itemsPool[i].amount*10/100
+            });
+            itemsPool1[i] = newData;
+            }
+
+            for(uint256 i=0; i< count ; i++){
+            Reward memory newData = Reward({
+             token :itemsPool[i].token,
+             amount : itemsPool[i].amount*30/100
+            });
+            itemsPool2[i] = newData;
+            }
+
+            for(uint256 i=0; i< count ; i++){
+            Reward memory newData = Reward({
+             token :itemsPool[i].token,
+             amount : itemsPool[i].amount*60/100
+            });
+            itemsPool3[i] = newData;
+            }
+            lastClaimTime = block.timestamp;
+            distributePool1(itemsPool1);
+            distributePool2(itemsPool2);
+            distributePool3(itemsPool3);
+            
+        }
+
+    
+
+    }
+
+    function distributePool1(Reward[] memory newData) private {
+        (address[] memory users, uint256[] memory _amount, 
+        uint256[] memory lastStaked, uint256 totalStaked) = 
+        IStaking(stakingContract).getAllHolder();
+        for(uint256 j = 0; j < users.length ; j++){
+            if(lastStaked[j]+epoch < block.timestamp)
+            {
+
+            for(uint256 i =0; i< newData.length;i++){
+            
+            IStaking(stakingContract).updateRewardsUser
+            (newData[i].token, (newData[i].amount*_amount[j])/(totalStaked));
+         }
+            }
+               
+            }
+        
+
+
+    }
+
+    function distributePool2(Reward[] memory newData) private {
+
+       (address[] memory users, uint256[] memory _amount, 
+        uint256[] memory lastStaked, uint256 totalStaked) = 
+        IStaking(stakingContract).getAllHolder();
+        for(uint256 j = 0; j < users.length ; j++){
+            if(lastStaked[j]+epoch < block.timestamp)
+            {
+
+            for(uint256 i =0; i< newData.length;i++){
+            
+            IStaking(stakingContract).updateRewardsUser
+            (newData[i].token, (newData[i].amount*_amount[j])/(totalStaked));
+         }
+            }
+               
+            }
+        
+    }
+
+    function distributePool3(Reward[] memory newData) private {
+
+        (address[] memory users, uint256[] memory _amount, 
+        uint256[] memory lastStaked, uint256 totalStaked) = 
+        IStaking(stakingContract).getAllHolder();
+        for(uint256 j = 0; j < users.length ; j++){
+            if(lastStaked[j]+epoch < block.timestamp)
+            {
+
+            for(uint256 i =0; i< newData.length;i++){
+            
+            IStaking(stakingContract).updateRewardsUser
+            (newData[i].token, (newData[i].amount*_amount[j])/(totalStaked));
+         }
+            }
+               
+            }
+        
     }
     
-    // function claimReward() external nonReentrant {
-    //     uint256 deposited = stakedBalance[msg.sender];
-    //     uint256 reward = deposited * (lastRewardRate - rewardRate[msg.sender]) / DIVISION; // reward = deposited * (S - S0[address]);
-    //     if (reward > 0) {
-    //         rewardToken.safeTransfer(msg.sender, reward);
-    //     }
-    //     rewardRate[msg.sender] = lastRewardRate;
-    //     emit ClaimReward(msg.sender, reward);
-    // }
-
-    // function rewardOf(address _account) public view returns (uint256){
-    //     uint deposited = stakedBalance[_account]; // deposited = stake[address];
-    //     return deposited * (lastRewardRate - rewardRate[_account]) / DIVISION; // reward = deposited * (S - S0[address]);
-    // }
 }
